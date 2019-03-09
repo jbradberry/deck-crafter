0005243d8c48a1ad24763732d6eb01d2db0b4f4e
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-03-09 17:40:54 -0500

    Add help text to the Card.tags field
    so that users know, when using the admin, that the tags are comma-separated.

--- deck_crafter/models.py
+++ deck_crafter/models.py
@@ -24,7 +24,10 @@ class Edition(models.Model):
 class Card(models.Model):
     edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
-    tags = ArrayField(models.CharField(max_length=64))
+    tags = ArrayField(
+        models.CharField(max_length=64),
+        help_text="Use a comma to separate tags in the edit form field.",
+    )
     text = models.TextField()
     image = models.ImageField(null=True)
     artist = models.CharField(max_length=256, blank=True)
