250ea0859a48f2e001713c18a8116b5455192d6b
beeankha <beeankha@gmail.com>
2019-03-08 23:37:03 -0500

    Update admin file with models

--- deck_crafter/admin.py
+++ deck_crafter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game
+from .models import Edition
+from .models import Card
+
+admin.site.register(Game)
+admin.site.register(Edition)
+admin.site.register(Card)
